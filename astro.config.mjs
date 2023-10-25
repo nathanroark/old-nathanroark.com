2085761e7765d244d3238a36e52878f32d185677
Nathan Roark <nathanroark@protonmail.com>
2023-10-25 13:51:42 -0500

    removed preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
-import preact from '@astrojs/preact'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), preact()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
